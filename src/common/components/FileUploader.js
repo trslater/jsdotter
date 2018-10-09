1a95eb400f0594e1b216b74cd0df00bec6284882
trslater <hello@trslater.ca>
2018-10-09 14:37:17 -0700

    Simplify file upload system

--- src/common/components/FileUploader.js
+++ src/common/components/FileUploader.js
@@ -1,39 +1,20 @@
 import React, { Component } from 'react'
 
 import Dropzone from 'react-dropzone'
-import FileList from './FileList'
 
 class FileUploader extends Component {
 	url = 'http://142.104.33.14:8001/upload/'
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			files: [],
-			seqNames: [],
-		}
-		this.selectedFiles = []
-	}
-
-	handleFileDrop(files) {
-		this.setState({ ...this.state, files: files })
-	}
-
-	handleListChange(event) {
-		this.selectedFiles = [].slice
-			.call(event.target.selectedOptions)
-			.map(opt => parseInt(opt.value))
-	}
-
 	// TODO: Seq name conflicts
-	async handleUploadClick() {
-		if (this.state.files.length > 0 && this.selectedFiles.length > 0) {
+	async handleFileDrop(files) {
+		if (files.length > 0) {
 			let seqNames = []
 
-			for (let i of this.selectedFiles) {
+			for (let file of files) {
+				console.log(file)
 				const body = new FormData()
 
-				body.append('file', this.state.files[i])
+				body.append('file', file)
 
 				const response = await fetch(
 					'http://142.104.33.14:8001/upload/',
@@ -60,14 +41,6 @@ class FileUploader extends Component {
 				<Dropzone onDrop={files => this.handleFileDrop(files)}>
 					<div style={{ marginTop: '20px' }}>Drop files here</div>
 				</Dropzone>
-				{/* TODO: Why do these need bind to make `this` work */}
-				<FileList
-					files={this.state.files}
-					onChange={this.handleListChange.bind(this)}
-				/>
-				<button onClick={this.handleUploadClick.bind(this)}>
-					Upload
-				</button>
 			</div>
 		)
 	}
