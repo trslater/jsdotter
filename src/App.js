21c0c14717276f7dd3afca2527102d2af4a21435
trslater <hello@trslater.ca>
2018-10-09 08:46:56 -0700

    Remove header for now (keep it simple)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.css'
 
-import Header       from './common/components/Header'
 import FileUploader from './common/components/FileUploader'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Header />
-				<FileUploader />
-				{/* TODO: Make these */}
-				{/* <SequenceList /> */}
-				{/* <AppButtons /> */}
+				<FileUploader onGetFiles={files => this.onGetFiles(files)} />
 			</div>
 		)
 	}
--- src/common/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () =>  (
-    <nav className="navbar">
-        <a href="/" className="navbar-brand">JDotter</a>
-    </nav>
-)
-
-export default Header
