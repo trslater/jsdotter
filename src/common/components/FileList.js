b275529b07fcac16b3ccb3e4377c00683462d864
trslater <hello@trslater.ca>
2018-10-09 11:25:33 -0700

    Greatly simplify FileList

--- src/common/components/FileList.js
+++ src/common/components/FileList.js
@@ -1,21 +1,12 @@
 import React from 'react'
 
-const FileList = props => {
-	const handleChange = event => {
-		let selected = event.target.selectedOptions
-
-		console.log(selected)
-	}
-
-	return (
-		<select onChange={handleChange} style={{ width: '200px' }} size={props.size} multiple>
-			{props.files.map((file, i) => (
-				<option key={i} value={file.name}>
-					{file.name}
-				</option>
-			))}
-		</select>
-	)
-}
+const FileList = props =>
+	<select style={{ width: '200px' }} size="10" multiple>
+		{props.files.map((file, i) => (
+			<option key={i} value={file.name}>
+				{file.name}
+			</option>
+		))}
+	</select>
 
 export default FileList
