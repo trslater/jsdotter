23508df8a1a6983c46d9394d7671360fc7b8156f
trslater <hello@trslater.ca>
2018-10-09 13:40:50 -0700

    Make a few small changes

--- src/common/components/FileList.js
+++ src/common/components/FileList.js
@@ -1,12 +1,20 @@
+// TODO: Combine with SequenceList
+
 import React from 'react'
 
-const FileList = props =>
-	<select onChange={props.onChange} style={{ width: '200px' }} size="10" multiple>
+const FileList = props => (
+	<select
+		onChange={props.onChange}
+		style={{ width: '200px' }}
+		size="10"
+		multiple
+	>
 		{props.files.map((file, i) => (
 			<option key={i} value={i}>
 				{file.name}
 			</option>
 		))}
 	</select>
+)
 
 export default FileList
