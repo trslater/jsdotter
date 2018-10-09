fe0beb4a38043538ea8e793dd14e0947941849e2
trslater <hello@trslater.ca>
2018-10-09 13:40:22 -0700

    Create sequence list component

---
+++ src/common/components/SequenceList.js
@@ -0,0 +1,15 @@
+// TODO: Combine with FileList
+
+import React from 'react'
+
+const SequenceList = props => (
+	<select style={{ width: '200px' }} size="10" multiple>
+		{props.seqNames.map((name, i) => (
+			<option key={i} value={i}>
+				{name}
+			</option>
+		))}
+	</select>
+)
+
+export default SequenceList
