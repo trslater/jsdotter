af9940314b4ca458947cddc0252199917a1f6639
trslater <hello@trslater.ca>
2018-10-15 14:20:58 -0700

    Implement more elegant this object handling

--- src/common/dev/DevTools.js
+++ src/common/dev/DevTools.js
@@ -1,7 +1,17 @@
-export const runDev = (thisObj, fn, ...args) => {
+/*
+runDev
+
+Usage:
+If you want to call the function with the proper this obj, you can pass it in bound.
+Example:
+```js
+runDev(this.myFunc.bind(this), a, b, c)
+```
+*/
+export const runDev = function(fn, ...args) {
 	if (typeof fn === 'function') {
 		if (process.env.NODE_ENV === 'development') {
-			fn.call(thisObj, ...args)
+			fn.call(window, ...args)
 		}
 	}
 }
