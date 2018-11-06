16ac06c7e253f4cccf7cb7ba69972e4e86e21095
trslater <hello@trslater.ca>
2018-11-06 12:22:25 -0800

    Format scrubber

--- src/common/components/HorizontalScrubber.js
+++ src/common/components/HorizontalScrubber.js
@@ -9,45 +9,47 @@ import log from 'common/dev/Logger'
 The scrubber takes its position in the form of a value
 */
 class HorizontalScrubber extends PureComponent {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+		super(props)
 
-        this.scrubberWidth = 30
-        this.minScrub      = 0
-        this.maxScrub      = this.props.width - this.scrubberWidth
-        this.minValue      = this.props.valueRange[0]
-        this.maxValue      = this.props.valueRange[1]
-    }
+		this.scrubberWidth = 30
+		this.minScrub = 0
+		this.maxScrub = this.props.width - this.scrubberWidth
+		this.minValue = this.props.valueRange[0]
+		this.maxValue = this.props.valueRange[1]
+	}
 
-    handleDrag(e, data) {
-        this.props.onScrub(this.scrubberPositionToValue(data.x))
-    }
+	handleDrag(e, data) {
+		this.props.onScrub(this.scrubberPositionToValue(data.x))
+	}
 
-    render() {
-        return (
-            <div style={{ width: this.props.width }} className={styles.track}>
-                <Draggable
-                    axis="x"
-                    bounds={{ left: this.minScrub, right: this.maxScrub }}
-                    onDrag={this.handleDrag.bind(this)}
-                    position={{ x: this.valueToScrubberPostion(this.props.value) }}
-                >
-                    <div
-                        style={{ width: this.scrubberWidth }}
-                        className={styles.scrubber}
-                    />
-                </Draggable>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div style={{ width: this.props.width }} className={styles.track}>
+				<Draggable
+					axis="x"
+					bounds={{ left: this.minScrub, right: this.maxScrub }}
+					onDrag={this.handleDrag.bind(this)}
+					position={{
+						x: this.valueToScrubberPostion(this.props.value),
+					}}
+				>
+					<div
+						style={{ width: this.scrubberWidth }}
+						className={styles.scrubber}
+					/>
+				</Draggable>
+			</div>
+		)
+	}
 
-    valueToScrubberPostion(value) {
-        return value * this.maxScrub / this.maxValue
-    }
+	valueToScrubberPostion(value) {
+		return (value * this.maxScrub) / this.maxValue
+	}
 
-    scrubberPositionToValue(position) {
-        return position * this.maxValue / this.maxScrub
-    }
+	scrubberPositionToValue(position) {
+		return (position * this.maxValue) / this.maxScrub
+	}
 }
 
 export default HorizontalScrubber
