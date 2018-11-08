dc5a8d0c4d3b3095be99ebd37c83b0c714d99e7c
trslater <hello@trslater.ca>
2018-11-08 12:36:15 -0800

    Fix: not all of xhairs draggable, and other small improvements

--- src/common/components/XYController.js
+++ src/common/components/XYController.js
@@ -4,15 +4,17 @@ import styles from './XYController.module.css'
 
 import Draggable from 'react-draggable'
 
-// TODO: Make surface clickable
+import log from 'common/dev/Logger'
+
 // TODO: New feature: axis markings
+// FIXME: Move xhairs to mouse click works, but it does not initiate drag
 
 // const x = Math.floor(this.state.horizSeqPosition / this.props.zoom)
 // 		const y = Math.floor(this.state.vertSeqPosition / this.props.zoom)
 
 const XYController = props => {
 	const xhairsOffset = props.xhairsSize / 2
-    
+
 	return (
 		<div className={styles.wrapper}>
 			<div
@@ -29,37 +31,59 @@ const XYController = props => {
 						right: props.width,
 						top: 0,
 						bottom: props.height,
-                    }}
-                    onDrag={props.onXhairsMove}
+					}}
+					onDrag={props.onXhairsMove}
 					position={{ x: props.x, y: props.y }}
 					allowAnyClick={true}
 				>
-					{/* FIXME: Only top-left, bottom-right portions of crosshairs draggable */}
 					<div
 						className={styles.xhairsWrapper}
 						style={{
-							width: props.xhairsSize / 2,
-							height: props.xhairsSize / 2,
+							width: props.xhairsSize,
+							height: props.xhairsSize,
+							marginLeft: -xhairsOffset,
+							marginTop: -xhairsOffset,
 						}}
 					>
-						<div
-                            className={styles.xhairsCursor}
-                            style={{
-                                width: props.xhairsSize / 2,
-							    height: props.xhairsSize / 2,
-                                marginLeft: -xhairsOffset,
-                                marginTop: -xhairsOffset,
-                            }}
-                        />
+						<XHairs xhairsSize={props.xhairsSize} />
 
-                        <div className={styles.coords}>
-                            ({props.xCoord}, {props.yCoord})
-                        </div>
+						<div className={styles.coords}>
+							({props.xCoord}, {props.yCoord})
+						</div>
 					</div>
 				</Draggable>
 			</div>
 			{props.children}
 		</div>
+	)
+}
+
+const XHairs = ({ xhairsSize }) => {
+	return (
+		<svg width={xhairsSize} height={xhairsSize}>
+			<line
+				shape-rendering="crispEdges"
+				x1="0"
+				y1="50%"
+				x2="100%"
+				y2="50%"
+				style={{
+					stroke: 'red',
+					strokeWidth: '1px',
+				}}
+			/>
+			<line
+				shape-rendering="crispEdges"
+				x1="50%"
+				y1="0"
+				x2="50%"
+				y2="100%"
+				style={{
+					stroke: 'red',
+					strokeWidth: '1px',
+				}}
+			/>
+		</svg>
 	)
 }
 
